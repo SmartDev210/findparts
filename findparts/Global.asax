ce1965ab9bf3a1bf0b5fc0332a17895180851c49
Smart Dev <79864567+SmartDev210@users.noreply.github.com>
2021-03-10 03:47:53 +0800

    Initial project create from asp.net mvc 5 with Identity library

---
+++ findparts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="findparts.MvcApplication" Language="C#" %>
---
+++ findparts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(findparts.Startup))]
+namespace findparts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
