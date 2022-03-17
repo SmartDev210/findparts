3f89549310ff989521e1aa4a628cd61c7f530c17
Smart Dev <79864567+SmartDev210@users.noreply.github.com>
2022-03-18 05:00:44 +0800

    calendly link updated

--- findparts/Services/Services/EmailService.cs
+++ findparts/Services/Services/EmailService.cs
@@ -731,7 +731,7 @@ namespace Findparts.Services.Services
 				+ "<br/><br/>"
 				+ $"Where Aviation Professionals Interview Aviation Brands."
 				+ "<br/><br/>"
-				+ @"For ✈️ Brands: <a href=""https://calendly.com/buddyaero"">Book a Mini Podcast</a>"
+				+ @"For ✈️ Brands: <a href=""https://calendly.com/aero-celebrity-podcast"">Book a Mini Podcast</a>"
 				+ "<br/>"
 				+ @"For ✈️ Creators: <a href=""https://docs.google.com/forms/d/e/1FAIpQLSfP-7FfvdhWNFKLd3jeCkwB3OVqxrSh2VjYWoCrlbkbKvuzlQ/viewform"">Get Paid to Share on LinkedIn</a>"
 				+ "<br/>"
